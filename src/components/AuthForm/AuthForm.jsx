ea21c01f1d7959a14f37c19aa940660b2a98a68b
aRyan111112 <formypcwork234@gmail.com>
2024-01-11 23:30:24 +0530

    Login feature added

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { Box, VStack,Image, Input, Button, Flex, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 function AuthForm() {
+  const [isLogin, setisLogin] = useState(true)
+
   return (
-    <div>AuthForm</div>
+    <Box border={"1px"} padding={4}>
+      <VStack gap={4}>
+        <Image src="/logo.png" h={24}/>
+        <Input
+        placeholder='Email'
+        fontSize={14}
+        type='email'
+      />
+        <Input
+        placeholder='Password'
+        fontSize={14}
+        type='password'
+      />
+
+      {!isLogin ? <Input placeholder='Confirm password'type='password' fontSize={14}/> : null}
+
+      <Button w={"full"} colorScheme='blue'>
+        {isLogin ? "Log in" : "Sign up"}
+      </Button>
+
+      {/* -------OR------- */}
+
+      <Flex w={"full"} justifyContent={"center"} alignItems={"center"} gap={2}>
+        <Box h={"2px"} bg={"gray.400"} flex={2}/>
+        <Text>OR</Text>
+        <Box h={"2px"} bg={"gray.400"} flex={2}/>
+      </Flex>
+
+      <Flex gap={2} cursor={"pointer"} my={"12px"}>
+        <Image src='/google.png' h={"24px"} alt='google logo' />
+        <Text color={"blue.500"}>Login with Google</Text>
+      </Flex>
+      </VStack>
+    </Box> 
   )
 }
 
