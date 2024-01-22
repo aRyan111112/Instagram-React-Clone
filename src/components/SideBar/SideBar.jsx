660956955e953dfe514b01b2c900f3680a4af8d2
aRyan111112 <formypcwork234@gmail.com>
2024-01-22 14:12:25 +0530

    project continued

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,8 +1,29 @@
+import { Box, Flex, Link } from '@chakra-ui/react'
 import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+import { InstagramLogo, InstagramMobileLogo } from '../../assets/constants'
 
 function SideBar() {
   return (
-    <div>SideBar</div>
+    <Box 
+    height={"100vh"}
+    borderRight={"1px solid"}
+    borderColor={"whiteAlpha.300"}
+    position={"sticky"}
+    top={0}
+    left={0}
+    py={8}
+    px={{base:2, md:4}}>
+
+      <Flex w={"full"} h={"full"} direction={'column'} gap={10}>
+        <Link as={RouterLink} to={"/"} pl={2} display={{base: "none", md: "block"}} cursor={"pointer"}><InstagramLogo/>
+        </Link>
+        <Link as={RouterLink} to={"/"} display={{base: "block", md: "none"}} cursor={"pointer"} p={2} borderRadius={6} _hover={{bg: "whiteAlpha.500"}} w={10}>
+          <InstagramMobileLogo/>
+        </Link>
+      </Flex>
+
+    </Box>  
   )
 }
 
